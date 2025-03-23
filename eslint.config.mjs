feaf8554e5e8a91c75dbb4f3de7d4c61b7174c5f
M.Cozzolino <michaelcozzolino@outlook.it>
2025-03-23 23:16:52 +0100

    feat: Add missing dependencies

---
+++ eslint.config.mjs
@@ -0,0 +1,3 @@
+import { michaelcozzolino } from './src/index.mjs';
+
+export default michaelcozzolino();
--- src/index.mjs
+++ src/index.mjs
@@ -1,62 +1,64 @@
 import path                  from 'node:path';
-import { fileURLToPath }     from 'node:url';
 import antfu                 from '@antfu/eslint-config';
 import { includeIgnoreFile } from '@eslint/compat';
+import cwd                   from '@stdlib/process-cwd';
 import alignImport           from 'eslint-plugin-align-import';
 
-export default antfu(
-    {
-        plugins: {
-            'align-import': alignImport,
+export function michaelcozzolino() {
+    return antfu(
+        {
+            plugins: {
+                'align-import': alignImport,
+            },
+            type:      'lib',
+            stylistic: {
+                indent: 4,
+                semi:   true,
+            },
+            ignores: includeIgnoreFile(path.resolve(cwd(), '.gitignore')).ignores,
         },
-        type:      'lib',
-        stylistic: {
-            indent: 4,
-            semi:   true,
-        },
-        ignores: includeIgnoreFile(path.resolve(path.dirname(fileURLToPath(import.meta.url)), '.gitignore')).ignores,
-    },
-    {
-        files: ['**/*.vue'],
-        rules: {
-            'vue/block-order': [
-                'error',
-                {
-                    order: ['template', 'script', 'style'],
-                },
-            ],
-            'vue/first-attribute-linebreak': [
-                'error',
-                {
-                    singleline: 'ignore',
-                    multiline:  'beside',
-                },
-            ],
-            'vue/no-empty-component-block': ['error'],
-        },
-    },
-    {
-        rules: {
-            '@stylistic/brace-style': ['error', '1tbs'],
-            '@stylistic/key-spacing': [
-                'error',
-                {
-                    align: { afterColon: true, beforeColon: false, on: 'value' },
-                },
-            ],
-            '@stylistic/no-multi-spaces': [
-                'error',
-                {
-                    exceptions: {
-                        Property:           true,
-                        ImportDeclaration:  true,
-                        TSTypeAnnotation:   true,
-                        VariableDeclarator: true,
+        {
+            files: ['**/*.vue'],
+            rules: {
+                'vue/block-order': [
+                    'error',
+                    {
+                        order: ['template', 'script', 'style'],
                     },
-                },
-            ],
-            'align-import/align-import': ['error'],
-            'align-import/trim-import':  ['error'],
+                ],
+                'vue/first-attribute-linebreak': [
+                    'error',
+                    {
+                        singleline: 'ignore',
+                        multiline:  'beside',
+                    },
+                ],
+                'vue/no-empty-component-block': ['error'],
+            },
         },
-    },
-);
+        {
+            rules: {
+                '@stylistic/brace-style': ['error', '1tbs'],
+                '@stylistic/key-spacing': [
+                    'error',
+                    {
+                        align: { afterColon: true, beforeColon: false, on: 'value' },
+                    },
+                ],
+                '@stylistic/no-multi-spaces': [
+                    'error',
+                    {
+                        exceptions: {
+                            Property:           true,
+                            ImportDeclaration:  true,
+                            TSTypeAnnotation:   true,
+                            VariableDeclarator: true,
+                        },
+                    },
+                ],
+                'align-import/align-import': ['error'],
+                'align-import/trim-import':  ['error'],
+            },
+        },
+    );
+}
